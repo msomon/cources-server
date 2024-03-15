0ea584f7c7edcc8cddf523425a0e4ed7040ebf61
unknown <hsomon940@gmail.com>
2024-03-15 01:27:53 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,110 @@
+
+# Food Shop
+
+## Testing Admin Panel :
+
+   Email:testing@gmail.com         
+   
+   Password:123456
+
+ 
+   ##   Live Site: https://foodshop1.vercel.app
+
+   ##  Server Code: https://github.com/msomon/foodshop-server
+
+   ##  Client Code: https://github.com/msomon/foodshop-clint
+
+
+Overview
+This repository contains the source code for a food shop website built using React. The website allows users to browse, select, and order food items. Below are the key features and instructions for setting up and running the project.
+
+Features
+1. Responsive Design
+The website is designed to provide a seamless user experience across various devices, including desktops, tablets, and mobile phones.
+
+2. User Authentication
+Users can create accounts, log in, and manage their profiles. Authentication ensures a secure and personalized shopping experience.
+
+
+4. Shopping Cart
+The website includes a shopping cart feature that allows users to add and remove items. The cart updates dynamically, providing users with a summary of their selected items.
+
+5. Checkout Process
+A simple and intuitive checkout process guides users through providing necessary information and completing their orders.
+
+6. Order History
+Registered users can view their order history, making it convenient to track past purchases and reorder favorite items.
+
+7. Admin Dashboard
+An admin dashboard is available for managing products, categories, and user orders. Admins can also view analytics and track sales.
+
+
+
+
+
+Contribution
+If you would like to contribute to the project, please follow the guidelines outlined in the CONTRIBUTING.md file.
+
+
+
+
+
+
+
+
+## 🚀 About Me
+Hello! I'm Sumon Ahamed, a passionate MERN stack web developer with a knack for turning ideas into reality through code. With a solid foundation in HTML, CSS, and JavaScript, I specialize in crafting dynamic and user-friendly web applications.
+
+💻 My expertise extends to React.js for building interactive user interfaces and Node.js for server-side development. I thrive on utilizing MongoDB and Firebase for database management, ensuring seamless data handling and retrieval.
+
+I also use the Next.js framework because it offers built-in support for API routes, enabling me to create backend functionality within the same project structure. Next.js builds upon the foundation of React, offering features such as server-side rendering (SSR) and static site generation (SSG) out of the box. This improves the performance and SEO-friendliness of web applications and provides a seamless development experience.
+
+🛠️ Additionally, I'm proficient in utilizing frameworks like Express.js for backend development and integrating UI libraries such as Tailwind CSS and Bootstrap to create visually stunning websites.
+
+🎨 Beyond coding, I bring creativity to the table with proficiency in design tools like Photoshop and Figma, enabling me to conceptualize and implement captivating user experiences.
+
+🔧 I'm well-versed in version control systems like GitHub for collaborative development and deployment platforms like Vercel for efficient project hosting.
+
+🚀 Whether it's developing robust web applications or refining user interfaces, I'm dedicated to delivering high-quality solutions that meet both client needs and industry standards.
+
+Let's connect and collaborate on exciting projects together!
+
+
+Github: https://github.com/msomon
+
+Portfolio: https://sumon-ahamed.vercel.app
+
+Contact Details :
+
+Email: Mdsumon123ahamed@gmail.com
+
+WhatsApp: +01748572428
+
+ ♦♦ THANK YOU ♦♦
+
+## Installation
+
+### Install my-project with npm
+ 
+
+Clone this repository to your local machine.
+
+### Run the command 
+
+git clone https://github.com/msomon/foodshop-clint
+
+git clone https://github.com/msomon/foodshop-server
+
+npm install 
+
+cd foodshop-clint ||  foodshop-server
+
+
+
+npm start
+Open your web browser and go to http://localhost:3000 to view the website.
+  
+
+
+
+
---
+++ index.js
@@ -0,0 +1,297 @@
+const express = require("express")
+const app = express()
+require("dotenv").config()
+const cors = require("cors")
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const port = process.env.PORT || 5000 ;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+//middelwere  //
+app.use(cors())
+app.use(express.json())
+var jwt = require('jsonwebtoken');
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lmsiai7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+
+
+
+
+function verifyJWT(req, res, next) {
+  // const authHeader = req.headers.authorization;
+  // console.log("res from jwt" , authHeader);
+  // console.log(req.headers.Authorization);
+  // if (!authHeader) {
+  //   return res.status(401).send({ message: 'UnAuthorized access' });
+  // }
+  // const token = authHeader.split(' ')[1];
+  // jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+  //   if (err) {
+  //     return res.status(403).send({ message: 'Forbidden access' })
+  //   }
+  //   req.decoded = decoded;
+  //   next();
+  // });
+}
+
+
+async function run() {
+ 
+    await client.connect();
+
+
+    const foodsCollection = client.db('cources').collection('cource')
+    const cartsCollection = client.db('cources').collection('carts')
+    const reviewsCollection = client.db('cources').collection('reviews')
+    const usersCollection = client.db('cources').collection('users')
+    const myprofileCollection = client.db('cources').collection('myprofiles')
+    const paymentCollection = client.db('cources').collection('payments')
+ 
+
+app.get("/cources", async (req,res)=>{
+    foods = await foodsCollection.find().toArray()
+    res.send(foods)
+})
+
+app.post("/addfooditem", async (req,res)=>{
+
+     const food = req.body
+    foods = await foodsCollection.insertOne(food)
+
+    res.send(foods)
+})
+
+app.delete("/deleteitem/:id", async (req,res)=>{
+
+     const id = req.params.id
+     const filter = {_id:new ObjectId(id)}
+    deleteItem = await foodsCollection.deleteOne(filter)
+
+    res.send(deleteItem)
+})
+
+
+
+
+
+app.get("/carts", async (req,res)=>{
+  const email= req.query.email
+  const filter = {email:email}
+    foods = await cartsCollection.find(filter).toArray()
+    res.send(foods)
+})
+
+app.put("/addToCart/:id", async(req,res)=>{
+
+  const id = req.params.id
+  const item = req.body
+  const filter ={_id:new ObjectId(id)}
+  const options = { upsert: true };
+  const updateDoc = {
+        $set:item,
+      };
+  const cartItem = await cartsCollection.updateOne(filter,updateDoc,options)
+  res.send(cartItem)
+
+})
+
+
+app.delete("/deleteCartItem/:id", async(req,res)=>{
+  
+ const id = req.params.id
+  const filter = {_id:new ObjectId(id)}
+  const cartItem = await cartsCollection.deleteOne(filter)
+  res.send(cartItem)
+
+})
+
+
+
+
+app.put("/updateCartItem/:id", async(req,res)=>{
+
+  const id = req.params.id
+  const item = req.body
+  const filter ={_id:new ObjectId(id)}
+  const options = { upsert: true };
+  const updateDoc = {
+        $set:item,
+      };
+  const cartItem = await cartsCollection.updateOne(filter,updateDoc,options)
+  res.send(cartItem)
+
+})
+
+
+app.get('/reviews',async(req,res)=>{
+  const reviews = await reviewsCollection.find().toArray()
+  res.send(reviews)
+})
+
+app.post('/addreview',async(req,res)=>{
+  const review = req.body
+    const result = await reviewsCollection.insertOne(review)
+    res.send(result)
+   
+})
+
+
+app.put('/user/:email', async (req, res) => {
+  const email = req.params.email;
+  const user = req.body;
+  const filter = { email: email };
+  const options = { upsert: true };
+  const updateDoc = {
+    $set: user,
+  };
+  const result = await usersCollection.updateOne(filter, updateDoc, options);
+  const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1d' })
+  res.send({ result, token });
+})
+
+
+app.get("/user/myprofile/:email",async (req,res)=>{
+  const email= req.params.email
+  const filter = {email:email}
+    profile = await myprofileCollection.findOne(filter)
+    res.send(profile)
+  
+})
+
+
+app.put("/user/myprofile/:email",async (req,res)=>{
+
+  const email = req.params.email;
+      const profile = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: profile,
+      };
+      const result = await myprofileCollection.updateOne(filter, updateDoc, options);
+      
+      res.send(result);
+
+
+})
+
+
+
+
+// payment//
+
+app.post('/create-payment-intent', async(req,res)=>{
+
+  const {totalSum} = req.body 
+  const totalAmount = (totalSum * 100) ;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount:totalAmount ,
+    currency: "usd",
+    payment_method_types:['card'],
+    
+  })
+  
+  res.send({clientSecret:paymentIntent.client_secret})
+  
+   })
+   
+  
+   app.post('/payment/:email',async(req,res)=>{
+     const email = req.params.email
+     const payment = req.body 
+     const filter ={email:email}
+     const deleteResult = await cartsCollection.deleteMany(filter)
+     const result = await paymentCollection.insertOne(payment)
+  res.send({deleteResult, result})
+   })
+
+
+  //  payment mathood close //
+
+
+  app.get('/users' ,async(req,res)=>{
+    users = await usersCollection.find().toArray()
+    res.send(users)
+  })
+
+  app.put("/users/admin/:email",async(req, res)=>{
+    const email = req.params.email;
+    const filter = { email: email };
+    const updateDoc = {
+      $set: { role: 'admin' },
+    };
+  
+    const admin = await usersCollection.updateOne(filter, updateDoc);
+    res.send(admin)
+  })
+
+  app.get("/users/admin/:email", async(req,res)=>{
+
+    const email = req.params.email;
+    const filter = { email: email };
+    const user = await usersCollection.findOne(filter);
+    const isAdmin = user?.role === 'admin';
+    res.send({admin: isAdmin})
+
+  })
+
+
+app.get("/allorders" ,async(req,res)=>{
+
+  const allorders = await paymentCollection.find().toArray()
+  res.send(allorders)
+
+} )
+
+app.get("/myorders/:email",async(req,res)=>{
+  const email= req.params.email
+  const filter = {email:email}
+  const myorders = await paymentCollection.find(filter).toArray()
+  res.send(myorders)
+
+})
+
+
+app.delete("/admincancelorder/:id",async(req,res)=>{
+  const id = req.params.id
+ 
+  const filter={_id:new ObjectId(id)}
+
+cancelorder = await paymentCollection.deleteOne(filter) 
+res.send(cancelorder)
+
+})
+
+
+
+
+
+  
+  }
+
+  
+
+
+
+
+run().catch(console.dir);
+
+
+app.get("/",(req,res) =>{
+res.send("Food shop server is running")
+})
+app.listen(port,()=>{
+    console.log(`server is running :${port}`);
+})
